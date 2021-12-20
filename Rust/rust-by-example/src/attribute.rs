452be215f81d8574d4daa43cb9ca4aa5810704e9
Wu Yongwei <wyw1191448318@hotmail.com>
2021-12-20 23:07:29 +0800

    rust-example: attributes
    --task=20211220

--- Rust/rust-by-example/src/attribute.rs
+++ Rust/rust-by-example/src/attribute.rs
@@ -1,4 +1,4 @@
-// `#[allow(dead_code)]` is an attribute 
+// `#[allow(dead_code)]` is an attribute
 // that disables the `dead_code` lint
 #[allow(dead_code)]
 fn unused_function() {}
@@ -7,7 +7,7 @@ fn unused_function() {}
 // This function only gets compiled if the target OS is linux
 #[cfg(target_os = "linux")]
 fn are_you_on_linux() {
-println!("You are running linux!");
+    println!("You are running linux!");
 }
 // This function only gets compiled if the target OS is not linux
 #[cfg(not(target_os = "linux"))]
@@ -18,7 +18,7 @@ fn are_you_on_linux() {
 fn conditional_check_test() {
     println!("==============> Conditional Check Test");
     are_you_on_linux();
-    
+
     if cfg!(target_os = "linux") {
         println!("It is definitely linux!");
     } else {
